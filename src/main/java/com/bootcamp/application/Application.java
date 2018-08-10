2062eceaa19c41b9f5166185af2d1781feb435fe
ibrahim00017 <hibrahimyya@gmail.com>
2018-08-10 09:08:33 +0100

    log4jIssu

--- src/main/java/com/bootcamp/application/Application.java
+++ src/main/java/com/bootcamp/application/Application.java
@@ -7,6 +7,7 @@
  */
 package com.bootcamp.application;
 
+import com.bootcamp.services.CommentaireService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -51,7 +52,11 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-
+        try {
+            new CommentaireService().createAllIndexCommentaire();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
